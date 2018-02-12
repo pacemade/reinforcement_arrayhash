43eefbefdf424e140a89507ac3e4306062ab0ceb
Danny Chang <dannychachang@gmail.com>
2018-02-12 13:07:43 -0500

    finish assignment

---
+++ reinforcement_domain.js
@@ -0,0 +1,5 @@
+var items = ["purchase domain", "install SSL certificate", "deploy site", "celebrate success"];
+for(var i = 0; i < items.length; i++){
+  console.log("Todo:");
+  console.log(items[i]);
+}
